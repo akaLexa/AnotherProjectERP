32e08652f71136d80a5e9a798aa7e251480faef2
akaLexa <epmak.g@gmail.com>
2016-11-19 13:29:21 +0300

    обновил дамб базы

--- configs/erp_db 20161112 0951.sql
+++ configs/erp_db 20161112 0951.sql
@@ -1,29 +1,102 @@
-﻿--
--- Script date 12.11.2016 9:51:10
+﻿-- Script date 19.11.2016 13:28:07
 -- Server version: 5.5.5-10.1.17-MariaDB
 -- Client version: 4.1
 --
 
 
--- 
+--
 -- Disable foreign keys
--- 
+--
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 
--- 
+--
 -- Set SQL mode
--- 
+--
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 
--- 
+--
 -- Set character set the client will use to send SQL statements to the server
 --
 SET NAMES 'utf8';
 
--- 
+--
 -- Set default database
 --
 USE erp_db;
+
+--
+-- Definition for table tbl_group_roles
+--
+DROP TABLE IF EXISTS tbl_group_roles;
+CREATE TABLE tbl_group_roles (
+  col_grID INT(11) NOT NULL AUTO_INCREMENT,
+  col_gID INT(11) DEFAULT NULL,
+  col_roleID INT(11) DEFAULT NULL,
+  PRIMARY KEY (col_grID),
+  CONSTRAINT FK_tbl_group_roles_tbl_user_groups_col_gID FOREIGN KEY (col_gID)
+    REFERENCES tbl_user_groups(col_gID) ON DELETE RESTRICT ON UPDATE RESTRICT,
+  CONSTRAINT FK_tbl_group_roles_tbl_user_roles_col_roleID FOREIGN KEY (col_roleID)
+    REFERENCES tbl_user_roles(col_roleID) ON DELETE RESTRICT ON UPDATE RESTRICT
+)
+ENGINE = INNODB
+AUTO_INCREMENT = 1
+CHARACTER SET utf8
+COLLATE utf8_general_ci
+COMMENT = 'какие роли к какой группе относятся';
+
+--
+-- Definition for table tbl_menu
+--
+DROP TABLE IF EXISTS tbl_menu;
+CREATE TABLE tbl_menu (
+  col_id INT(11) NOT NULL AUTO_INCREMENT,
+  col_mtitle VARCHAR(255) DEFAULT NULL,
+  col_mtype INT(11) DEFAULT NULL,
+  col_link VARCHAR(255) DEFAULT NULL,
+  col_modul VARCHAR(255) DEFAULT NULL,
+  col_Seq INT(11) DEFAULT NULL,
+  PRIMARY KEY (col_id)
+)
+ENGINE = INNODB
+AUTO_INCREMENT = 1
+CHARACTER SET utf8
+COLLATE utf8_general_ci;
+
+--
+-- Definition for table tbl_menu_type
+--
+DROP TABLE IF EXISTS tbl_menu_type;
+CREATE TABLE tbl_menu_type (
+  col_id INT(11) NOT NULL AUTO_INCREMENT,
+  col_ttitle VARCHAR(255) DEFAULT NULL,
+  col_tbuild VARCHAR(255) DEFAULT NULL,
+  col_seq INT(11) DEFAULT NULL,
+  PRIMARY KEY (col_id)
+)
+ENGINE = INNODB
+AUTO_INCREMENT = 1
+CHARACTER SET utf8
+COLLATE utf8_general_ci;
+
+--
+-- Definition for table tbl_module_roles
+--
+DROP TABLE IF EXISTS tbl_module_roles;
+CREATE TABLE tbl_module_roles (
+  col_mrID INT(11) NOT NULL AUTO_INCREMENT,
+  col_modID INT(11) DEFAULT NULL,
+  col_roleID INT(11) DEFAULT NULL,
+  PRIMARY KEY (col_mrID),
+  CONSTRAINT FK_tbl_module_roles_tbl_modules_col_modID FOREIGN KEY (col_modID)
+    REFERENCES tbl_modules(col_modID) ON DELETE RESTRICT ON UPDATE RESTRICT,
+  CONSTRAINT FK_tbl_module_roles_tbl_user_roles_col_roleID FOREIGN KEY (col_roleID)
+    REFERENCES tbl_user_roles(col_roleID) ON DELETE RESTRICT ON UPDATE RESTRICT
+)
+ENGINE = INNODB
+AUTO_INCREMENT = 6
+CHARACTER SET utf8
+COLLATE utf8_general_ci
+COMMENT = 'указание ролей с правами доступа';
 
 --
 -- Definition for table tbl_modules
@@ -34,12 +107,11 @@ CREATE TABLE tbl_modules (
   col_title VARCHAR(255) DEFAULT NULL COMMENT 'идентификатор названия страницы',
   col_path VARCHAR(255) DEFAULT NULL COMMENT 'местонахождения скрипта',
   col_cache INT(11) DEFAULT 0 COMMENT 'кеширование в секундах',
-  col_isOn CHAR(1) DEFAULT '1' COMMENT 'состояние on/off',
   col_isClass CHAR(1) DEFAULT '1' COMMENT 'mvc или обычный скрипт',
   PRIMARY KEY (col_modID)
 )
 ENGINE = INNODB
-AUTO_INCREMENT = 1
+AUTO_INCREMENT = 2
 CHARACTER SET utf8
 COLLATE utf8_general_ci
 COMMENT = 'модули системы';
@@ -82,62 +154,6 @@ COLLATE utf8_general_ci
 COMMENT = 'группы пользователей';
 
 --
--- Definition for table tbl_user_roles
---
-DROP TABLE IF EXISTS tbl_user_roles;
-CREATE TABLE tbl_user_roles (
-  col_roleID INT(11) NOT NULL AUTO_INCREMENT,
-  col_roleName VARCHAR(250) DEFAULT NULL,
-  PRIMARY KEY (col_roleID)
-)
-ENGINE = INNODB
-AUTO_INCREMENT = 2
-AVG_ROW_LENGTH = 16384
-CHARACTER SET utf8
-COLLATE utf8_general_ci
-COMMENT = 'роли для пользователя';
-
---
--- Definition for table tbl_group_roles
---
-DROP TABLE IF EXISTS tbl_group_roles;
-CREATE TABLE tbl_group_roles (
-  col_grID INT(11) NOT NULL AUTO_INCREMENT,
-  col_gID INT(11) DEFAULT NULL,
-  col_roleID INT(11) DEFAULT NULL,
-  PRIMARY KEY (col_grID),
-  CONSTRAINT FK_tbl_group_roles_tbl_user_groups_col_gID FOREIGN KEY (col_gID)
-    REFERENCES tbl_user_groups(col_gID) ON DELETE RESTRICT ON UPDATE RESTRICT,
-  CONSTRAINT FK_tbl_group_roles_tbl_user_roles_col_roleID FOREIGN KEY (col_roleID)
-    REFERENCES tbl_user_roles(col_roleID) ON DELETE RESTRICT ON UPDATE RESTRICT
-)
-ENGINE = INNODB
-AUTO_INCREMENT = 1
-CHARACTER SET utf8
-COLLATE utf8_general_ci
-COMMENT = 'какие роли к какой группе относятся';
-
---
--- Definition for table tbl_module_roles
---
-DROP TABLE IF EXISTS tbl_module_roles;
-CREATE TABLE tbl_module_roles (
-  col_mrID INT(11) NOT NULL AUTO_INCREMENT,
-  col_modID INT(11) DEFAULT NULL,
-  col_roleID INT(11) DEFAULT NULL,
-  PRIMARY KEY (col_mrID),
-  CONSTRAINT FK_tbl_module_roles_tbl_modules_col_modID FOREIGN KEY (col_modID)
-    REFERENCES tbl_modules(col_modID) ON DELETE RESTRICT ON UPDATE RESTRICT,
-  CONSTRAINT FK_tbl_module_roles_tbl_user_roles_col_roleID FOREIGN KEY (col_roleID)
-    REFERENCES tbl_user_roles(col_roleID) ON DELETE RESTRICT ON UPDATE RESTRICT
-)
-ENGINE = INNODB
-AUTO_INCREMENT = 1
-CHARACTER SET utf8
-COLLATE utf8_general_ci
-COMMENT = 'указание ролей с правами доступа';
-
---
 -- Definition for table tbl_user_role_rule
 --
 DROP TABLE IF EXISTS tbl_user_role_rule;
@@ -154,19 +170,59 @@ CHARACTER SET utf8
 COLLATE utf8_general_ci
 COMMENT = 'роли, что назначены пользователю';
 
--- 
+--
+-- Definition for table tbl_user_roles
+--
+DROP TABLE IF EXISTS tbl_user_roles;
+CREATE TABLE tbl_user_roles (
+  col_roleID INT(11) NOT NULL AUTO_INCREMENT,
+  col_roleName VARCHAR(250) DEFAULT NULL,
+  PRIMARY KEY (col_roleID)
+)
+ENGINE = INNODB
+AUTO_INCREMENT = 7
+AVG_ROW_LENGTH = 2730
+CHARACTER SET utf8
+COLLATE utf8_general_ci
+COMMENT = 'роли для пользователя';
+
+--
+-- Dumping data for table tbl_group_roles
+--
+
+-- Table erp_db.tbl_group_roles does not contain any data (it is empty)
+
+--
+-- Dumping data for table tbl_menu
+--
+
+-- Table erp_db.tbl_menu does not contain any data (it is empty)
+
+--
+-- Dumping data for table tbl_menu_type
+--
+
+-- Table erp_db.tbl_menu_type does not contain any data (it is empty)
+
+--
+-- Dumping data for table tbl_module_roles
+--
+
+-- Table erp_db.tbl_module_roles does not contain any data (it is empty)
+
+--
 -- Dumping data for table tbl_modules
 --
 
 -- Table erp_db.tbl_modules does not contain any data (it is empty)
 
--- 
+--
 -- Dumping data for table tbl_user
 --
 
 -- Table erp_db.tbl_user does not contain any data (it is empty)
 
--- 
+--
 -- Dumping data for table tbl_user_groups
 --
 INSERT INTO tbl_user_groups VALUES
@@ -175,29 +231,22 @@ INSERT INTO tbl_user_groups VALUES
 (3, 'Пользователь'),
 (4, 'Все');
 
--- 
--- Dumping data for table tbl_user_roles
 --
-INSERT INTO tbl_user_roles VALUES
-(1, 'Администратор системы');
-
--- 
--- Dumping data for table tbl_group_roles
---
-
--- Table erp_db.tbl_group_roles does not contain any data (it is empty)
-
--- 
--- Dumping data for table tbl_module_roles
---
-
--- Table erp_db.tbl_module_roles does not contain any data (it is empty)
-
--- 
 -- Dumping data for table tbl_user_role_rule
 --
 
 -- Table erp_db.tbl_user_role_rule does not contain any data (it is empty)
+
+--
+-- Dumping data for table tbl_user_roles
+--
+INSERT INTO tbl_user_roles VALUES
+(1, 'Администратор системы'),
+(2, 'тестовая роль 1'),
+(3, 'тестовая роль 2'),
+(4, 'тестовая роль 3'),
+(5, 'тестовая роль 4'),
+(6, 'тестовая роль 5');
 
 -- 
 -- Restore previous SQL mode
