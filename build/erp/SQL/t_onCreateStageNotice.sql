d81ca9169ca62bbc5b880ae7b9e27696b969d1d1
akaLexa <epmak.g@gmail.com>
2017-03-19 14:16:50 +0300

    некоторые изменения в стандартном тексте к событиям

--- build/erp/SQL/t_onCreateStageNotice.sql
+++ build/erp/SQL/t_onCreateStageNotice.sql
@@ -7,9 +7,6 @@ FOR EACH ROW
   BEGIN
     IF new.col_statusID != 5 THEN
       INSERT INTO tbl_events (col_etID, col_object, col_userID, col_comment)
-        VALUE (1, new.col_projectID, new.col_respID, (SELECT
-                                                        col_StageName
-                                                      FROM tbl_hb_project_stage
-                                                      WHERE col_StageID = new.col_stageID));
+        VALUE (1, new.col_projectID, new.col_respID, CONCAT( (SELECT col_StageName FROM tbl_hb_project_stage WHERE col_StageID = new.col_stageID), '. Проект: ',(SELECT CONCAT(col_pnID,'. ',col_projectName)  FROM tbl_project WHERE col_projectID = new.col_projectID)));
     END IF;
   END
