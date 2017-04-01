a22207380d4a651342f2c13d4d5d38088a8925f2
akaLexa <epmak.g@gmail.com>
2017-04-01 08:03:03 +0300

    люблю свою IDE, которая иногда делает очень больно...

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
  * ->
  **/
 
-require __DIR__ . DIRECTORY_SEPARATOR;
+require __DIR__ . DIRECTORY_SEPARATOR . 'mwce' . DIRECTORY_SEPARATOR . 'Routing' . DIRECTORY_SEPARATOR . 'Autoload.php';
 
 $app = mwce\Routing\Router::start();
 $app->startPlugins();
