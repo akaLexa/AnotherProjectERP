f1ef9fcf0522be80bbc61e8a66de902051fefb71
akaLexa <epmak.g@gmail.com>
2017-05-27 14:23:45 +0300

    визуальные поправки в карточке пользователя небольшой рефакторнигш

--- build/erp/plugins/Login.php
+++ build/erp/plugins/Login.php
@@ -64,7 +64,7 @@ class Login extends PluginController
                 session_destroy();
                 Tools::go();
             }
-            //default
+
             $this->view
                 ->set('uid',(file_exists(baseDir . DIRECTORY_SEPARATOR . 'theme' . DIRECTORY_SEPARATOR . 'imgs' . DIRECTORY_SEPARATOR . 'users' . DIRECTORY_SEPARATOR . Configs::userID() . '.png') ? Configs::userID() : 'default'))
                 ->out('LoginIn','plugin_'.$this->className);
